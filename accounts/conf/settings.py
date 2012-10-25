ababeed8851cb43418e5cc9c9e275a15f4abed74
Lefford <lefford.girigori@gmail.com>
2012-10-25 13:36:56 +0200

    Add path and database configuration

--- accounts/conf/settings.py
+++ accounts/conf/settings.py
@@ -1,4 +1,8 @@
 # Django settings for accounts project.
+import os
+
+PROJECT_PATH = os.path.join(os.path.abspath(__file__), os.pardir)
+ROOT_PATH = os.path.join(PROJECT_PATH, os.pardir)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,8 +15,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(ROOT_PATH, 'accounts.db'), # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
