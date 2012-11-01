9879b4e85465276a4ed0100c4e372a9ae3f2e1d2
Lefford <lefford.girigori@gmail.com>
2012-11-01 11:29:55 +0100

    Removed unused classes and attributes

--- accounts/importer/models.py
+++ accounts/importer/models.py
@@ -1,15 +1,22 @@
 from django.db import models
-from accounts.account.models import Company
 
-class Resource(models.Model):
-    class Meta:
-        abstract = True
-        
-    partner = models.ForeignKey(Company)
-    resource_type = models.CharField(max_length=255)
-
-class FlightTicket(Resource):
-    pass
-
-class Hotel(Resource):
-    pass
+class Account(models.Model):
+    resource_uri = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    birth_date = models.DateField()
+    gender = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    street_name = models.CharField(max_length=255)
+    zipcode = models.CharField(max_length=255)
+    lead = models.BooleanField()
+    mailing_list = models.CharField(max_length=200)
+    phone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    tr_referral = models.CharField(max_length=255)
+    utm_medium = models.CharField(max_length=255)
+    utm_source = models.CharField(max_length=255)
+    
+    def __unicode__(self):
+        return u' '.join([self.first_name, self.last_name])
