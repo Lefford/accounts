98bf6832eb3eee70d8574eb1e478dd2ca1f6e878
Lefford <lefford.girigori@gmail.com>
2012-11-01 11:31:23 +0100

    Add client cache and url builder

--- accounts/interface/client.py
+++ accounts/interface/client.py
@@ -1,18 +1,19 @@
-from restclient.clients.jsonclient import JSONClient
+import urllib
+
 from accounts.utils.utils import url_join
+from restclient.clients.jsonclient import JSONClient
 
 class ImporterClient(JSONClient):
     
-    def __init__(self, base_url):
+    def __init__(self, base_url=u'', url_params=None):
         self.base_url = base_url
+        self.url_params = url_params
+        # enable cache default is 2 hour
+        super(ImporterClient, self).__init__('.cache')
         
     def request(self, uri, method='GET', body=None, headers=None, redirections=5, connection_type=None):
         if not uri.startswith(self.base_url):
             uri = url_join(self.base_url, uri)
-        JSONClient.request(self, uri, method=method, body=body, headers=headers, redirections=redirections, connection_type=connection_type)
-
-    
-        
-    
-        
-        
+        if self.url_params:
+            uri = "?".join([uri, urllib.urlencode(self.url_params)])
+        return super(ImporterClient, self).request(uri, method, body, headers, redirections, connection_type)
